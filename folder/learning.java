d1537dec4f5711913076138e701012d7d4b9c7eb
piyushchoudhary2001 <57358377+piyushchoudhary2001@users.noreply.github.com>
2023-04-07 19:45:45 +0530

    Add files via upload

---
+++ folder/learning.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class learning {
+
+    JFrame  frame;
+    learning()
+    {
+        frame=new JFrame("My Window");
+        frame.setVisible(true);
+        frame.setBounds(300,300,500,300);
+        
+    }
+    public static void main(String[] args) {
+         new learning();
+    }
+}
